817ed69a7bfe98c685a67d11e51dd12d625a8448
themanforfree <themanforfree@gmail.com>
2020-09-15 22:19:21 +0800

    Update

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def main():
     """
     主函数
     """
+    content = ''
     for j in range(len(dept_texts)):
         stu_name = stu_names[j]
         stu_id = stu_ids[j]
@@ -116,6 +117,7 @@ def main():
 
         # 提交打卡与结果判定
         flag = 0
+        flag2 = 0
         for i in range(1, 5):
             print('成员{0}第{1}次尝试打卡中...'.format(j+1, i))
             response = requests.post(check_url, json=check_json)
@@ -132,30 +134,31 @@ def main():
                 msg = time_msg + '时' + stu_name + "打卡成功"
             else:
                 msg = time_msg + "打卡异常"
+                flag2 = 1
         else:
             msg = time_msg + "网络错误打卡失败"
+            flag2 = 1
 
         print(msg)
-
+        content += msg + "  \n"
         # 微信通知
-        title = msg
-        result = json.dumps(response.json(), sort_keys=True,
-                            indent=4, separators=(',', ': '), ensure_ascii=False)
-        content = f"""
-        ```
-        {result}
-        ```
-
-        """
-        data = {
-            "text": title,
-            "desp": content
-        }
-        response = requests.post(sc_url, data=data)
-        if response.status_code == 200:
-            print('Server酱推送成功!')
+        if flag2 == 1:
+            title = "打卡异常，存在打卡失败情况，请手动打卡"
         else:
-            print('Server酱推送失败!')
+            title = "全部成员打卡成功"
+
+    wechat_push(sc_url, title, content)
+
+def wechat_push(sc_url, title, content):
+    data = {
+        "text": title,
+        "desp": content
+    }
+    response = requests.post(sc_url, data=data)
+    if response.status_code == 200:
+        print('Server酱推送成功!')
+    else:
+        print('Server酱推送失败!')
 
 
 def print_info_error():
